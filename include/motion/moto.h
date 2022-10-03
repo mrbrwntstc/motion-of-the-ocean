ea4387d12703906f5bcdd26623c04d83319ea640
gbaby <frame_0f_mind@outlook.com>
2022-10-03 10:56:09 -0500

    global import header file

---
+++ include/motion/moto.h
@@ -0,0 +1,12 @@
+/**
+ * Global import header 
+ */
+
+#ifndef __MOTION_MOTO_H__
+#define __MOTION_MOTO_H__
+
+#include "core.h"
+#include "particle.h"
+#include "precision.h"
+
+#endif // __MOTION_MOTO_H__
